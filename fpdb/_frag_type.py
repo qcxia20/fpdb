3937ff5b0314a0d6290d532e9a63e3d7adbe60d1
yangjincai <yangjincai@nibs.ac.cn>
2017-11-02 17:14:04 +0800

    add H with default bond length

--- fpdb/_frag_type.py
+++ fpdb/_frag_type.py
@@ -9,56 +9,56 @@ bck = [(1,'N'),(1,'H'),(0,'C'),(0,'O'),
        (1,'CA'),(1,'HA'),(1,'C'),(1,'CB')]
 
 wtr = {'HOH':['OW','HW1','HW2']}
-alc = {'THR':['CB', ('CA','CB', 0.9), ('CG2','CB',0.9), 'HB',
+alc = {'THR':['CB', ('CA','CB', 1.09), ('CG2','CB',1.09), 'HB',
               'OG1', 'HG1'],  
-       'SER':['CB', ('CA','CB', 0.9), 'HB1', 'HB2', 'OG', 'HG']}
+       'SER':['CB', ('CA','CB', 1.09), 'HB1', 'HB2', 'OG', 'HG']}
 
-arg = {'ARG':['CZ', 'NE', 'NH1', 'NH2', ('CD','NE', 0.9), 'HE', 
+arg = {'ARG':['CZ', 'NE', 'NH1', 'NH2', ('CD','NE', 1.0), 'HE', 
               'HH11', 'HH12', 'HH21', 'HH22']}
 ben = {'PHE':['CG', 'CD1', 'CE1', 'CZ', 'CE2', 'CD2', 
-             ('CB','CG',0.9), 'HD1', 'HE1', 'HZ', 'HE2', 'HD2']}
+             ('CB','CG',1.09), 'HD1', 'HE1', 'HZ', 'HE2', 'HD2']}
 
-co2 = {'ASP':['CB',('CA', 'CB', 0.9),'HB1','HB2','CG','OD1','OD2'],
-       'GLU':['CG',('CB', 'CG', 0.9),'HG1','HG2','CD','OE1','OE2']}
+co2 = {'ASP':['CB',('CA', 'CB', 1.09),'HB1','HB2','CG','OD1','OD2'],
+       'GLU':['CG',('CB', 'CG', 1.09),'HG1','HG2','CD','OE1','OE2']}
 
-cys = {'CYS':['CB',('CA','CB', 0.9),'HB1','HB2','SG','HG']}
+cys = {'CYS':['CB',('CA','CB', 1.08),'HB1','HB2','SG','HG']}
 
-gln = {'ASN':['CB','CG','OD1','ND2',('CA','CB',0.9),
+gln = {'ASN':['CB','CG','OD1','ND2',('CA','CB',1.09),
               'HB1','HB2','HD21','HD22'],
-       'GLN':['CG','CD','OE1','NE2',('CB','CG',0.9),
+       'GLN':['CG','CD','OE1','NE2',('CB','CG',1.09),
               'HG1','HG2','HE21','HE22']}
 
 hid = {'HID':['CE1','ND1','CG','CD2','NE2',
-              'HE1','HD1',('CB','CG',0.9),'HD2'],
+              'HE1','HD1',('CB','CG',1.07),'HD2'],
        'HIE':['CE1','NE2','CD2','CG','ND1',
-              'HE1','HE2','HD2',('CB','CG',0.9)]} 
+              'HE1','HE2','HD2',('CB','CG',1.07)]} 
 
 hip = {'HIP':['CE1','NE2','CD2','CG','ND1',
-              'HE1','HE2','HD2',('CB','CG',0.9),'HD1']}
+              'HE1','HE2','HD2',('CB','CG',1.07),'HD1']}
 
-met = {'MET':['CG',('CB','CG',0.9),'HG1','HG2','SD',
+met = {'MET':['CG',('CB','CG',1.09),'HG1','HG2','SD',
               'CE','HE1','HE2','HE3']}
 
 nh4 = {'LYS':['NZ','HZ1','HZ2','HZ3',
-              'CE','HE1','HE2',('CD','CE', 0.9)]}
+              'CE','HE1','HE2',('CD','CE', 1.08)]}
 
-prp = {'ARG':['CB',('CA','CB',0.9),'HB1','HB2','CG','HG1','HG2',
-              'CD','HD1','HD2',('NE','CD',0.9)],
-       'ILE':['CB',('CA','CB',0.9),('CG2','CB',0.9),'HB',
+prp = {'ARG':['CB',('CA','CB',1.09),'HB1','HB2','CG','HG1','HG2',
+              'CD','HD1','HD2',('NE','CD',1.09)],
+       'ILE':['CB',('CA','CB',1.09),('CG2','CB',1.09),'HB',
               'CG1','HG11','HG12','CD','HD1','HD2','HD3'],
-       'LEU':['CD1','HD11','HD12','HD13','CG',('CB','CG',0.9),'HG',
+       'LEU':['CD1','HD11','HD12','HD13','CG',('CB','CG',1.09),'HG',
               'CD2','HD21','HD22','HD23'],
-       'LYS':['CB',('CA','CB',0.9),'HB1','HB2','CG','HG1','HG2',
-              'CD','HD1','HD2',('CE','CD',0.9)],
-       'PRO':['CB',('CA','CB',0.9),'HB1','HB2','CG','HG1','HG2',
-              'CD','HD1','HD2',('N','CD',0.9)],
-       'VAL':['CG1','HG11','HG12','HG13','CB',('CA','CB',0.9),'HB',
+       'LYS':['CB',('CA','CB',1.09),'HB1','HB2','CG','HG1','HG2',
+              'CD','HD1','HD2',('CE','CD',1.09)],
+       'PRO':['CB',('CA','CB',1.09),'HB1','HB2','CG','HG1','HG2',
+              'CD','HD1','HD2',('N','CD',1.09)],
+       'VAL':['CG1','HG11','HG12','HG13','CB',('CA','CB',1.09),'HB',
               'CG2','HG21','HG22','HG23']}
 
 trp = {'TRP':['CG','CD1','CD2','NE1','CE2','CE3','CZ2','CZ3','CH2',
-              'HD1','HE1','HE3','HZ2','HZ3','HH2',('CB','CG',0.9)]}
+              'HD1','HE1','HE3','HZ2','HZ3','HH2',('CB','CG',1.08)]}
 tyr = {'TYR':['CE1','CD1','CG','CD2','CE2','CZ','OH',
-              'HE1','HD1',('CB','CG',0.9),'HD2','HE2','HH']}
+              'HE1','HD1',('CB','CG',1.08),'HD2','HE2','HH']}
 
 lnk = {
   'ALA':['CB','1HB','2HB','3HB'],
