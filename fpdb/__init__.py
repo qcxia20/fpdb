d54ab91964390610d2118d358fa5ea6a30fcd786
jcyang <gold.yang@qq.com>
2018-10-12 13:12:27 +0800

    add setup.py

--- fpdb/__init__.py
+++ fpdb/__init__.py
@@ -1,2 +1,10 @@
-from .fpdb import *
-from .frotate import *
+"""
+Imports all submodules
+"""
+from __future__ import division
+from __future__ import unicode_literals
+
+__version__ = '0.1.0'
+
+from fpdb.fpdb import *
+from fpdb.frotate import *
